77e9f7f27f66924370595a5ad5315dbce066c487
naveenkosigi <naveenkosigi@gmail.com>
2021-09-11 16:46:57 +0530

    added ingredients in recipe details

--- src/app/MODELS/recipe.model.ts
+++ src/app/MODELS/recipe.model.ts
@@ -1,11 +1,15 @@
+import { ingredient } from "./ingredient.model";
+
 export class recipe{
     public name:string;
     public description:string;
     public url:string;
+    public ingredients:ingredient[];
 
-    constructor(name:string,description:string,url:string){
+    constructor(name:string,description:string,url:string,ingredients:ingredient[]){
         this.name=name;
         this.description=description;
         this.url=url;
+        this.ingredients=ingredients;
     }
 }
