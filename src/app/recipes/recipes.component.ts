be1d72bc9751b3c4e66aedbf2c7db5f4db3fb519
NAVEEN KOSGI <naveenkosigi@gmail.com>
2021-09-29 21:08:54 +0530

    recipe list service will be injected newly on recipes tab click

--- src/app/recipes/recipes.component.ts
+++ src/app/recipes/recipes.component.ts
@@ -6,7 +6,8 @@ import { recipeListService } from '../services/recipe-list.service';
 @Component({
   selector: 'app-recipes',
   templateUrl: './recipes.component.html',
-  styleUrls: ['./recipes.component.css']
+  styleUrls: ['./recipes.component.css'],
+  providers:[recipeListService]
 })
 export class RecipesComponent implements OnInit {
   selectedRecipe:recipe;
